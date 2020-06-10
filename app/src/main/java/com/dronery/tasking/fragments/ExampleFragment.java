fb34d9cfb7f45a87d54cf36b4fbca4705bbf7f15
Anibal Abello <a.abellohernandez@gmail.com>
2020-06-10 14:09:07 -0400

    commit inicial

--- app/src/main/java/com/dronery/tasking/fragments/ExampleFragment.java
+++ app/src/main/java/com/dronery/tasking/fragments/ExampleFragment.java
@@ -1,7 +1,14 @@
 package com.dronery.tasking.fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +29,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ExampleFragment extends DefaultFragment {
 
@@ -33,10 +41,12 @@ public class ExampleFragment extends DefaultFragment {
     String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
     String nameStr;
     FloatingActionButton mail;
+    AlertDialog.Builder builder;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = super.onCreateView(inflater, container, savedInstanceState);
-
+        builder = new AlertDialog.Builder(getContext());
         listVIew = (ListView) root.findViewById(R.id.idListView);
         button = (Button) root.findViewById(R.id.btnAddItem); //new
 
@@ -58,68 +68,134 @@ public class ExampleFragment extends DefaultFragment {
     }
 
     private void setUpListViewListener() {
-        listVIew.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Context context = getActivity().getApplicationContext();
+        try{
 
-                nameStr = parent.getItemAtPosition(position).toString();
-
-                Toast.makeText(context, "Actividad "+nameStr+" eliminada", Toast.LENGTH_LONG).show();
-                items.remove(position);
-                itemsAdapter.notifyDataSetChanged();
+            listVIew.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
 
-                lista.getTextos().remove(nameStr);
-                firestoreManager.saveObject(FirestoreManager.FS_COLLECTION_USUARIOS, userId, lista, task1 -> {
+                    Context context = getActivity().getApplicationContext();
+
+                    nameStr = parent.getItemAtPosition(position).toString();
+
+                    //Toast.makeText(context, R.string.word_task +" "+ nameStr +" "+ R.string.word_removed, Toast.LENGTH_LONG).show(); //no funciona
+                    Toast.makeText(context, R.string.alert_deleted_succesful, Toast.LENGTH_LONG).show();
+
+                    items.remove(position);
+                    itemsAdapter.notifyDataSetChanged();
+
+
+                    lista.getTextos().remove(nameStr);
+                    firestoreManager.saveObject(FirestoreManager.FS_COLLECTION_USUARIOS, userId, lista, task1 -> {
                         itemsAdapter.remove(nameStr);
-                });
+                    });
 
-                return true;
-            }
-        });
+                    return true;
+                }
+            });
+
+        }catch(Throwable e){
+            e.printStackTrace();
+            builder.setTitle(R.string.alert_header).setMessage(R.string.alert_error_delete_item)
+                    .setNeutralButton(R.string.alert_button_ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+                            testEmail();
+                            dialog.cancel();
+                        }
+                    })
+                    .show();
+        }
+    }
+
+    public void testEmail(){
+        final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{ getContext().getResources().getString(R.string.alert_mail_dev) });
+        intent.putExtra(Intent.EXTRA_SUBJECT, getContext().getResources().getString(R.string.email_subject));
+        intent.putExtra(Intent.EXTRA_TEXT, getContext().getResources().getString(R.string.email_body) +
+                getContext().getResources().getString(R.string.code_email_ExampleFragment));
+        intent.setType("text/plain");
+        final PackageManager pm = getActivity().getPackageManager();
+        final List<ResolveInfo> matches = pm.queryIntentActivities(intent, 0);
+        ResolveInfo best = null;
+        for (final ResolveInfo info : matches)
+            if (info.activityInfo.packageName.endsWith(".gm") ||
+                    info.activityInfo.name.toLowerCase().contains("gmail")) best = info;
+        if (best != null)
+            intent.setClassName(best.activityInfo.packageName, best.activityInfo.name);
+        startActivity(intent);
     }
 
 
     public void loadUserFirebaseData() {
-        firestoreManager.getDocument(FirestoreManager.FS_COLLECTION_USUARIOS, userId, task -> {
-            if(!task.isSuccessful()){
-                return;
-            }
-            DocumentSnapshot documento = task.getResult();
-            lista = documento.toObject(Actividades.class);
+        try{
+            firestoreManager.getDocument(FirestoreManager.FS_COLLECTION_USUARIOS, userId, task -> {
+                if(!task.isSuccessful()){
+                    return;
+                }
+                DocumentSnapshot documento = task.getResult();
+                lista = documento.toObject(Actividades.class);
 
-            if(lista == null){
-                lista = new Actividades();
-            }
+                if(lista == null){
+                    lista = new Actividades();
+                }
 
-            if(lista.getTextos() == null){
-                lista.setTextos(new ArrayList<>());
+                if(lista.getTextos() == null){
+                    lista.setTextos(new ArrayList<>());
 //                lista.setEstado(new ArrayList<>());
-            }
+                }
 
-            for (int i=0; i<= lista.getTextos().size() -1; i++){
+                for (int i=0; i<= lista.getTextos().size() -1; i++){
 //                if(lista.getEstado().get(i) == false) {
                     itemsAdapter.add(lista.getTextos().get(i));
 //                }
-            }
-        });
-    }
-
-    private void addItem() {
-        EditText input = getActivity().findViewById(R.id.etNewItem);
-        String itemText = input.getText().toString();
-
-
-        if(!(itemText.equals(""))){
-            lista.getTextos().add(itemText);
-            firestoreManager.saveObject(FirestoreManager.FS_COLLECTION_USUARIOS, userId, lista, task1 -> {
-                itemsAdapter.add(itemText);
-                input.setText("");
+                }
             });
+        }catch(Throwable e){
+            e.printStackTrace();
+            builder.setTitle(R.string.alert_header).setMessage(R.string.alert_error_get_document)
+                    .setNeutralButton(R.string.alert_button_ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+                            testEmail();
+                            dialog.cancel();
+                        }
+                    })
+                    .show();
         }
-        else{
-            Toast.makeText(getActivity().getApplicationContext(), "Please enter text.. ", Toast.LENGTH_LONG).show();
+
+    }
+//
+    private void addItem() {
+        try{
+            EditText input = getActivity().findViewById(R.id.etNewItem);
+            String itemText = input.getText().toString();
+
+            if(!(itemText.equals(""))){
+                lista.getTextos().add(itemText);
+                firestoreManager.saveObject(FirestoreManager.FS_COLLECTION_USUARIOS, userId, lista, task1 -> {
+                    itemsAdapter.add(itemText);
+                    input.setText("");
+                });
+
+                Toast.makeText(getContext(), R.string.alert_succesful_firebase,Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(getActivity().getApplicationContext(), R.string.alert_empty_task, Toast.LENGTH_LONG).show();
+            }
+        }catch(Throwable e){
+            e.printStackTrace();
+            builder.setTitle(R.string.alert_header).setMessage(R.string.alert_error_add_item)
+                    .setNeutralButton(R.string.alert_button_ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+                            testEmail();
+                            dialog.cancel();
+                        }
+                    })
+                    .show();
         }
     }
 
